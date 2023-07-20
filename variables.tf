1b632730f1578f8759deb94aa16ce29db20345ff
Nelson <nelsonrp24@users.noreply.github.com>
2023-07-20 13:31:58 +0200

    variables

--- variables.tf
+++ variables.tf
@@ -1,16 +1,16 @@
 # Resource Group Variables
 
-variable "az_rg_name" {
-  description = "The Name of the Resource Group"
+variable "rg_name" {
+  description = "The Name which should be used for this Resource Group. Changing this forces a new Resource Group to be created."
   type        = string
 }
 
-variable "az_rg_location" {
-  description = "The Azure Region where the Resource Group should exist"
+variable "rg_location" {
+  description = "The Azure Region where the Resource Group should exist. Changing this forces a new Resource Group to be created."
   type        = string
 }
 
-variable "az_tags" {
+variable "rg_tags" {
   description = "A mapping of tags which should be assigned to all resources"
   type        = map(any)
   default     = {}
