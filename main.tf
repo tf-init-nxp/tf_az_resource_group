9f8ef8cc886fef4c0758c534e5bcdaedca2a4f65
nelsonrp24 <nelsonrp24@gmail.com>
2023-08-07 15:22:41 +0200

    Developing product implementation

--- main.tf
+++ main.tf
@@ -2,12 +2,3 @@ locals {
   default_tags = {}
   all_tags     = merge(local.default_tags, var.rg_tags)
 }
-
-# Resource Group
-
-resource "azurerm_resource_group" "resource_group" {
-  name     = var.rg_name
-  location = var.rg_location
-
-  tags = local.all_tags
-}
--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,9 @@
 output "rg_name" {
-  value = azurerm_resource_group.resource_group.name
+  value       = azurerm_resource_group.resource_group.name
+  description = "Resource Group Name"
 }
 
 output "rg_location" {
-  value = azurerm_resource_group.resource_group.location
+  value       = azurerm_resource_group.resource_group.location
+  description = "Resource Group Location"
 }
